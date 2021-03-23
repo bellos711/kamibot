ce1c21b8bd8284fdfd3f6397c6fe9dd0ee731df4
Kahlil Bello <bellos711@gmail.com>
2021-03-22 23:13:18 -0700

    updated commands to have subfolders

--- commands/random/arguments.js
+++ commands/random/arguments.js
@@ -1,14 +1,16 @@
 module.exports = {
 	name: 'arguments',
 	description: 'testing arguments here reply!',
+	args: true,
+	usage: '<args1> <args2>',
 	execute(message, args) {
 		//COMMAND WITH ARGUMENTS
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
+		// if (!args.length) {
+		// 	return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		// }
 
 		if(args[0] === 'bark'){
-			return message.channel.send(`Watdo?!`);
+			return message.channel.send(`Classy bark!`);
 		}
 	
 		message.channel.send(`You said: ${message}\nThe arguments you said was: ${args}`);
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.replies = new Discord.Collection();
 
+const commandFolders = fs.readdirSync('./commands');
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); 
 /*
 The fs.readdirSync() method will return an array of all the file names in that directory, e.g. ['kami.js', 'bark.js']. The filter is there to make sure any non-JS files are left out of the array. With that array, you can loop over it and dynamically set your commands to the Collection you made above.
@@ -19,14 +21,12 @@ const replyFiles = fs.readdirSync('./replies').filter(file => file.endsWith('.js
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
-	console.log(file);
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
 }
 
 //---------------------------------------
@@ -68,65 +68,37 @@ client.on('message', message => {
 
 	//IF YOU HAVE A COMMAND + ARGUMENTS
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
 	
 
 	console.log(message.content);
 
 	//If there isn't a command with that name, exit early.
-	if (!client.commands.has(command)) return;
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+
+	//if command takes an argument, but no length so no argument provided
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments 🤔, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
 
 	//If there is, .get() the command and call its .execute() method while passing in your message and args variables as the method arguments.
 	try {
-		client.commands.get(command).execute(message, args);
+		command.execute(message, args);
 	} catch (error) {
 		//In case something goes wrong, log the error and report back to the member to let them know.
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
 
-	//------------------------------------------------
-
-	
-
-
-	// if(command === 'kami') {
-	// 	client.commands.get('kami').execute(message, args);
-	// } else if (command === 'bark') {
-	// 	client.commands.get('bark').execute(message, args);
-	// } else if (command === 'kick') {
-	// 	client.commands.get('kick').execute(message, args);
-	// } else if (command === 'delete') {
-	// 	client.commands.get('delete').execute(message, args);
-	// } else if (command === 'avatar') {
-	// 	client.commands.get('avatar').execute(message, args);
-	// }
-
-	// let lowercase_msg = message.content.toLowerCase();
-
-	// if (lowercase_msg === 'halo') {
-	// 	message.channel.send(`HALO ${message.author.username}`);
-
-	// } else if (lowercase_msg === 'kami'){
-	// 	message.channel.send('I AM ALMIGHTY KAMI');
-	// } else if (lowercase_msg.includes(`god`)){
-	// 	message.channel.send(`Did someone summon me?`);
-	// } else if (lowercase_msg === `${prefix}serverinfo`){
-	// 	message.channel.send(`Server is: ${message.guild.name}, \nTotal Members: ${message.guild.memberCount}`);
-	// } else if (command === 'arguments++') {
-	// 	//COMMAND WITH ARGUMENTS
-	// 	if (!args.length) {
-	// 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	// 	}
-
-	// 	if(args[0] === 'bark'){
-	// 		return message.channel.send(`Watdo?!`);
-	// 	}
-	
-	// 	message.channel.send(`You said: ${command}\nThe arguments you said was: ${args}`);
-	// 	message.channel.send(`First argument being: ${args[0]}`);
-	// } 
 
 });
 
